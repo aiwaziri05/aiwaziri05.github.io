91129204552987fa3614e201f04337f083c7896b
Abdullahi Waziri <idiwaziriabdullahi@gmail.com>
2023-07-31 16:42:42 +0100

    initial commit

---
+++ class_of_23/js/code.js
@@ -0,0 +1,17 @@
+let empty = document.forms["form1"]["text1"].value,
+    text_box = document.getElementById("text-box"),
+    submit_button = document.getElementById("submit-button"),
+    name = "Abdullahi Waziri";
+  
+    
+
+    function myFunction() {
+      
+        alert("hello")
+      
+    }
+    
+      
+
+    
+    myFunction();
