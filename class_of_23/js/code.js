acbadae3efc09a689b35b8e4c53c486dae532265
Abdullahi Waziri <idiwaziriabdullahi@gmail.com>
2023-08-01 10:39:19 +0100

    project completed

--- class_of_23/js/code.js
+++ class_of_23/js/code.js
@@ -2,15 +2,18 @@ function formValidation() {
     let validate = document.forms["myForm"]["userName"].value,
     text_box = document.getElementById("text-box"),
     nameErr = document.getElementById("nameErr"),
+    input_field = document.getElementById("input-field"),
     name = "Abdullahi Waziri";
 
     if(validate == "") {
        nameErr.innerHTML = "Name must be filled out";
         return false;
     }else if(validate != "") {
-       text_box.innerHTML = "Congratulations on Your Graduation " + validate + "Wishing you all the best on your new beginning" + name;
+       text_box.innerHTML = "Congratulations on Your Graduation " + validate + " Wishing you all the best on your new beginning </br> " + "By " + name;
+       
+       return false;
     }
-    
+
 }
 
 // formValidation();
